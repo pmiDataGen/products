1b5639dca5f0e127bfdd43eea0cd04d2ffb1fa63
vikas.e.mishra <vikas.e.mishra@BDC7-L-1Q4PMC2.dir.svc.accenture.com>
2019-02-13 20:29:17 +0530

    Initial project

---
+++ dataGenerator/src/main/java/com/pmi/DataGeneratorApplication.java
@@ -0,0 +1,14 @@
+package com.pmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataGeneratorApplication.class, args);
+	}
+
+}
+
