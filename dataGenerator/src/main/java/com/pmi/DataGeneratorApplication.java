20219a47b79811c83dfbf984c4661384e566db67
Boja Prasath <boja.p.ramalingam@accenture.com>
2019-02-21 12:31:37 +0530

    Added set property for Persona.java

--- dataGenerator/src/main/java/com/pmi/DataGeneratorApplication.java
+++ dataGenerator/src/main/java/com/pmi/DataGeneratorApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
 
+import com.pmi.pojo.Persona;
 import com.pmi.pojo.Identities;
 import com.pmi.pojo.PersonaRequest;
 import com.pmi.pojo.Cases;
@@ -64,13 +65,87 @@ public class DataGeneratorApplication {
 		// invokeRestService.callADLLookupAPI("cases");
 		// invokeRestService.callADLLookupAPI("orders");
 
+		// Write API - Personas
 		
-		  // Write API - Personas
+		Persona persona = new Persona();
+		persona.setOnline_access_flag(false);
+		persona.setPersona_id("66");
+		persona.setFirst_name("Test");
+		persona.setLast_name("Test");
+		persona.setFull_name("Test");
+		persona.setName("Test");
+		persona.setPhone("66");
+		persona.setEmail("test@pmi.com");
+		persona.setGender("M");
+		persona.setHome_country("Test");
+		persona.setIs_deleted(false);
+		persona.setBlocked_flag(false);
+		persona.setConsumer_type("Test");
+		persona.setDate_of_birth("1548716400000.0");
+		persona.setSegment("Smoker");
+		persona.setRegistration_date(66);
+		persona.setFirst_login_date("1548716400000.0");
+		persona.setLast_update_date("1548716400000.0");
+		persona.setLast_login_date("1548716400000.0");
+		persona.setOnline_access_flag(false);
+		persona.setPrimary_log_in("Test");
+		persona.setSystem_last_update("1548716400000.0");
+		persona.setLast_Activity_since_days(6);
+		persona.setLast_Activity_since_months(9);
+		persona.setAge(66);
+		persona.setAge_group("test");
+		persona.setRecency_score(66);
+		persona.setFrequency_score(66);
+		persona.setValue_score(66);
+		persona.setLast_case_category_name("Test");
+		persona.setLast_case_start_date("1548716400000.0");
+		persona.setLast_case_status_code("Test");
+		persona.setNb_cases_close(6);
+		persona.setNb_cases_close_1m(66);
+		persona.setNb_cases_close_2m(66);
+		persona.setNb_cases_close_3m(66);
+		persona.setNb_cases_close_6m(66);
+		persona.setNb_cases_close_12m(66);
+		persona.setNb_cases_open(66);
+		persona.setNb_cases_open_1m(66);
+		persona.setNb_cases_open_2m(66);
+		persona.setNb_cases_open_3m(66);
+		persona.setNb_cases_open_6m(66);
+		persona.setNb_cases_open_12m(66);
+		persona.setNb_active_chargers(66);
+		persona.setLimited_edition("Test");
+		persona.setNb_of_devices(66);
+		persona.setNb_of_devices_replaced(66);
+		persona.setNb_of_holders(66);
+		persona.setLatest_version_of_device("Test");
+		persona.setNumber_of_heet(66);
+		persona.setLifetime_revenue(66);
+		persona.setFirst_purchase_date(154871);
+		persona.setLast_order_date(1234);
+		persona.setNumber_of_accessories(66);
+		persona.setNb_order(66);
+		persona.setNb_order_average_per_month(66);
+		persona.setRegistration_first_order_difference_day(66);
+		persona.setSecond_order_difference_days(66);
+		persona.setTotal_spending_per_month_in_average(66);
+		persona.setCnt_orders(66);
+		persona.setCnt_orders_l12m(66);
+		persona.setCnt_orders_l3m(66);
+		persona.setCnt_orders_l6m(66);
+		persona.setRevenue_ytd(66);
+		persona.setRevenue_l3m(66);
+		persona.setRevenue_l6m(66);
+		persona.setRevenue_l12m(66);
+		
+		invokeRestService.callADLWriteAPI("personas", persona);
+		
+		  // Write API - PersonasRequest
 		 /* 
 		  PersonaRequest persona = new PersonaRequest();
 		  persona.setTd_c360_operation("put"); persona.setPersona_id("100");
 		  persona.setLast_name("mishra"); persona.setFirst_name("vikas");
 		  persona.setGender("male"); persona.setHome_country("india");
+		  
 		  invokeRestService.callADLWriteAPI("personas", persona);
 		 */
 
@@ -111,35 +186,25 @@ public class DataGeneratorApplication {
 		 * cases.setSubject_code(66); cases.setSubject_description(66);
 		 * cases.setLatest_update_date("Test"); cases.setHome_country("Test");
 		 * 
-		 * 
 		 * invokeRestService.callADLWriteAPI("cases", cases);
 		 */
 		
 		//Write API - Orders
-		
-		
-		  Orders orders = new Orders(); 
-		  orders.setTd_c360_operation("put");
-		  orders.setPersona_id("66"); 
-		  orders.setOrder_id("66");
-		  orders.setOrder_item_identifier("66"); 
-		  orders.setOrder_items("66");
-		  orders.setCountry("Test");
-		  orders.setOrder_amount(66);
-		  orders.setOrder_discount(66);
-		  orders.setOrder_type("test");
-		  orders.setOrder_status("test");
-		  orders.setOrder_date("1548716400000.0");
-		  orders.setOrder_currency("Test");
-		  orders.setItem_identifier("66");
-		  orders.setProduct_variant("test");
-		  orders.setItem_description("test");
-		  orders.setItem_price(66);
-		  orders.setItem_quantity(66);
-		  orders.setHome_country("test");
-		  orders.setStatus("test");
-		  
-		  invokeRestService.callADLWriteAPI("orders", orders);
+		/*
+		 * Orders orders = new Orders(); orders.setTd_c360_operation("put");
+		 * orders.setPersona_id("66"); orders.setOrder_id("66");
+		 * orders.setOrder_item_identifier("66"); orders.setOrder_items("66");
+		 * orders.setCountry("Test"); orders.setOrder_amount(66);
+		 * orders.setOrder_discount(66); orders.setOrder_type("test");
+		 * orders.setOrder_status("test"); orders.setOrder_date("1548716400000.0");
+		 * orders.setOrder_currency("Test"); orders.setItem_identifier("66");
+		 * orders.setProduct_variant("test"); orders.setItem_description("test");
+		 * orders.setItem_price(66); orders.setItem_quantity(66);
+		 * orders.setHome_country("test"); orders.setStatus("test");
+		 * 
+		 * invokeRestService.callADLWriteAPI("orders", orders);
+		 * 
+		 */  
 	}
 
 }
