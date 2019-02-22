b11191bd6fa268c946d2c4afc83156479de582c9
vikas.e.mishra <vikas.e.mishra@BDC11-L-7200173.dir.svc.accenture.com>
2019-02-22 16:10:50 +0530

    added fields

--- dataGenerator/src/main/java/com/pmi/pojo/Device.java
+++ dataGenerator/src/main/java/com/pmi/pojo/Device.java
@@ -17,6 +17,14 @@ public class Device {
 	private String home_country;
 	private String apiCallTimeTakenInMillis;
 
+	// Below fields received from POSTMAN response
+	private String identity_id;
+	private String model;
+	private String persona_id;
+	private String device_serial_number;
+	// private String time;
+	// private String td_c360_operation_time; // td:c360:operation_time
+
 	public Device() {
 		System.out.println("This is Device - Default Constructor");
 	}
@@ -25,22 +33,16 @@ public class Device {
 		this.device_codentify = device_codentify;
 	}
 
-	public Device(String td_c360_operation, String device_codentify, String identity_unique_identifier, String persona_identifier,
-			String device_type, String device_version, Date registration_device_date, String status,
-			String component_Code, Date status_date_change, Date end_of_warranty_date, String home_country, String apiCallTimeTakenInMillis) {
-		this.td_c360_operation = td_c360_operation;;
-		this.device_codentify = device_codentify;
-		this.identity_unique_identifier = identity_unique_identifier;
-		this.persona_identifier = persona_identifier;
-		this.device_type = device_type;
-		this.device_version = device_version;
-		this.registration_device_date = registration_device_date;
-		this.status = status;
-		this.component_Code = component_Code;
-		this.status_date_change = status_date_change;
-		this.end_of_warranty_date = end_of_warranty_date;
-		this.home_country = home_country;
-		this.apiCallTimeTakenInMillis = apiCallTimeTakenInMillis;
+	@Override
+	public String toString() {
+		return "Device [td_c360_operation=" + td_c360_operation + ", device_codentify=" + device_codentify
+				+ ", identity_unique_identifier=" + identity_unique_identifier + ", persona_identifier="
+				+ persona_identifier + ", device_type=" + device_type + ", device_version=" + device_version
+				+ ", registration_device_date=" + registration_device_date + ", status=" + status + ", component_Code="
+				+ component_Code + ", status_date_change=" + status_date_change + ", end_of_warranty_date="
+				+ end_of_warranty_date + ", home_country=" + home_country + ", apiCallTimeTakenInMillis="
+				+ apiCallTimeTakenInMillis + ", identity_id=" + identity_id + ", model=" + model + ", persona_id="
+				+ persona_id + ", device_serial_number=" + device_serial_number + "]";
 	}
 
 	public String getDevice_codentify() {
@@ -147,5 +149,36 @@ public class Device {
 		this.td_c360_operation = td_c360_operation;
 	}
 
-	
+	public String getIdentity_id() {
+		return identity_id;
+	}
+
+	public void setIdentity_id(String identity_id) {
+		this.identity_id = identity_id;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getPersona_id() {
+		return persona_id;
+	}
+
+	public void setPersona_id(String persona_id) {
+		this.persona_id = persona_id;
+	}
+
+	public String getDevice_serial_number() {
+		return device_serial_number;
+	}
+
+	public void setDevice_serial_number(String device_serial_number) {
+		this.device_serial_number = device_serial_number;
+	}
+
 }
